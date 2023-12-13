dba008ffdde463529f6c9b0d55c34d310e12f9d0
Ahadjon Nosirov <anosirov-vel@transparent.com>
2023-12-13 20:04:25 +0500

    cool

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SignUpComponent} from './component/UI/sign-up/sign-up.component';
+import {SignInComponent} from './component/UI/sign-in/sign-in.component';
+import {BaseComponent} from './component/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BaseComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+
+
+  {path: '**', redirectTo: '', component: BaseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {SignInComponent} from './component/UI/sign-in/sign-in.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {SignUpComponent} from './component/UI/sign-up/sign-up.component';
+import {HeaderComponent} from './component/UI/header/header.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FooterComponent} from './component/UI/footer/footer.component';
+import {BaseComponent} from './component/base/base.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
+
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    HeaderComponent,
+    FooterComponent,
+    BaseComponent,
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
